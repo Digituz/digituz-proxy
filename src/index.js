707e2b7137de00692f141607a323e7519b540d9b
Bruno Krebs <bruno.krebs@auth0.com>
2018-04-21 00:12:27 -0300

    creating a proxy for Digituz containers;

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+  const { host } = req.headers;
+  const subdomain = host.substring(0, host.indexOf('.digituz.com.br'));
+  proxy.web(req, res, { target: `http://${subdomain}` });
+});
+
+server.listen(3000);
