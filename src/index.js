78b1ac18959d813b0cc8093d3ede28f9410195a6
Bruno Krebs <bruno.krebs@auth0.com>
2018-04-21 00:44:52 -0300

    trying to be resilient;

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  const { host } = req.headers;
-  const subdomain = host.substring(0, host.indexOf('.digituz.com.br'));
-  proxy.web(req, res, { target: `http://${subdomain}` });
+  try {
+    const { host } = req.headers;
+    const subdomain = host.substring(0, host.indexOf('.digituz.com.br'));
+    proxy.web(req, res, { target: `http://${subdomain}` });
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
 
 server.listen(3000, () => {
